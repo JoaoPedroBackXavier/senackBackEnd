ea945cd150feea5cec56d538e906eec7f27db997
JoaoPedroBackXavier <joaopedro.backxavier@gmail.com>
2023-07-19 20:19:39 -0300

    runing api

---
+++ senack-backend(api)/apiCodeFast/readme.md
@@ -0,0 +1,2 @@
+cadastro de pessoas em banco de dados .json.
+o sistema deve listar usuarios, cadastrar, editar e remover
---
+++ senack-backend(api)/apiCodeFast/src/server.ts
@@ -0,0 +1,4 @@
+import app from './app'
+
+// iniciar o servidor
+app.app.listen(3333, () => console.log("running in door 3333"))
