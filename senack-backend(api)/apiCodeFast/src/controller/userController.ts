2f2b1d2aebaba8ac319e587f1ebb8fb225433c14
JoaoPedroBackXavier <joaopedro.backxavier@gmail.com>
2023-07-21 20:09:01 -0300

    ready

--- senack-backend(api)/apiCodeFast/src/controller/userController.ts
+++ senack-backend(api)/apiCodeFast/src/controller/userController.ts
@@ -4,13 +4,54 @@ const fs = require('fs');
 
 const data: string = "./dataBase.json"
 
+export default {
+    listUsers,
+    registerUsers,
+    editUser,
+    deletUser
+}
+
 async function listUsers(req: Request, res: Response) {
         const jsonData = fs.readFileSync(data)
         res.send(JSON.parse(jsonData))
 }
+async function registerUsers(req: Request, res: Response) {
+    const jsonData = fs.readFileSync(data)
+    let content = JSON.parse(jsonData)
+    let index: number = Object.keys(content).length
+    content[index++] = req.body
 
+    const values = JSON.stringify(content)
 
+    fs.writeFileSync(data, values)
 
-export default {
-    listUsers
+    res.status(201).send("registrado")
 }
+async function editUser(req: Request, res: Response) {
+    const jsonData = fs.readFileSync(data)
+    
+    const userId = req.params.id
+
+    let content = JSON.parse(jsonData)
+
+    content[userId] = req.body
+
+    const values = JSON.stringify(content)
+
+    fs.writeFileSync(data, values)
+
+    res.send(`updated ${userId}`)
+
+}
+async function deletUser(req: Request, res: Response) {
+    const jsonData = fs.readFileSync(data)   
+    const userId = req.params.id
+    let content = JSON.parse(jsonData)
+    delete content[userId]
+    const values = JSON.stringify(content)
+    fs.writeFileSync(data, values)
+    res.send(`user witch id ${userId} has ben deleted`)
+}
+
+
+
--- senack-backend(api)/apiCodeFast/src/routes/userRouter.ts
+++ senack-backend(api)/apiCodeFast/src/routes/userRouter.ts
@@ -7,51 +7,14 @@ const router = express.Router()
 // listar
 router.get('/users', userController.listUsers)
 
-/*
+
 // cadastrar usuarios
-router.post('/users', (req: any, res: any) =>
-{
-    const jsonData = fs.readFileSync(data)
-    let content = JSON.parse(jsonData)
-
-    // verifica a quantidade de objetos na base de dados
-    let index: number = Object.keys(content).length
-    content[index++] = req.body
-
-    const values = JSON.stringify(content)
-
-    fs.writeFileSync(data, values)
-
-    res.status(201).send("registrado")
-})
+router.post('/users', userController.registerUsers)
 
 // editar usuarios
-router.put('/user/:id', (req: any, res: any) => {
-    const jsonData = fs.readFileSync(data)
-    
-    const userId = req.params.id
-
-    let content = JSON.parse(jsonData)
-
-    content[userId] = req.body
-
-    const values = JSON.stringify(content)
-
-     fs.writeFileSync(data, values)
-
-    res.send(`updated ${userId}`)
-
-})
+router.put('/user/:id', userController.editUser)
 
 // deletar usuarios
-router.delete('/user/:id', (req: any, res: any) => {
-    const jsonData = fs.readFileSync(data)   
-    const userId = req.params.id
-    let content = JSON.parse(jsonData)
-    delete content[userId]
-    const values = JSON.stringify(content)
-    fs.writeFileSync(data, values)
-    res.send(`user witch id ${userId} has ben deleted`)
-})
-*/
+router.delete('/user/:id', userController.deletUser)
+
 export default router
